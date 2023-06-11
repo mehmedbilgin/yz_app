03fc480640ae8f617e2e74c9661ad2859c40864c
mehmedbilgin <mehmedbilgin7@gmail.com>
2023-06-12 01:21:46 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace yz_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Georgia", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox1.Location = new System.Drawing.Point(316, 121);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 24);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Playlist";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Georgia", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox2.Location = new System.Drawing.Point(439, 121);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(122, 24);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Single Song";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(592, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Müzik Önerisi Al";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(256, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(206, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(431, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Müzik Öneri Sistemine Hoşgeldiniz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(9, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(451, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Beğendiğiniz bir müzik veya playlistin linkini aşağıya bırakın ardından ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(259, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(317, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sistem Tarafınfan Oluşturulan Öneriniz";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(274, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "ne bıraktığınızı işaretleyip butona tıklayın.";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(106, 186);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(624, 268);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 489);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.Text = "Music Recommendation System";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yz_app
+{
+    public partial class Form1 : Form
+    {
+        public string link;
+        public Boolean music;
+        public Boolean playlist;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MakeApiRequest(textBox1.Text);
+            /*
+            music = checkBox2.Checked;
+            playlist = checkBox1.Checked;
+            link = textBox1.Text;
+            if(music==true && playlist==false)
+            {
+                var restClient = new RestClient("http://127.0.0.1:5000/");
+                var request = new RestRequest("music_recommendation",Method.Get);
+                request.AddQueryParameter("link",link);
+                var response = restClient.Execute<List<response_model>>(request);
+                List<response_model> result = response.Data;
+                MessageBox.Show(response.Data.ToString());
+                dataGridView1.DataSource = result;
+                //label4.Text = response.Data;
+
+            }else if(music==false && playlist == true)
+            {
+                var restClient = new RestClient("http://127.0.0.1:5000/");
+                var request = new RestRequest("recommend", Method.Post);
+                request.AddQueryParameter("playlist_id", link);
+                var response = restClient.Execute<List<response_model>>(request);
+                List<response_model> result = response.Data;
+                MessageBox.Show(response.Data.ToString());
+                dataGridView1.DataSource = result;
+                //label4.Text = response.Data;
+            }
+            else
+            {
+                MessageBox.Show("Hatalı bir seçim yaptınız, lütfen seçim yapıp tekrar deneyiniz.");
+            }*/
+        }
+        private const string ApiUrl = "http://localhost:5000/recommend";
+        public async void MakeApiRequest(string playlistId)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // API isteği için JSON verisini oluşturma
+                var requestData = new { playlist_id = playlistId };
+                var json = JsonConvert.SerializeObject(requestData);
+                var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+                // API isteği gönderme
+                var response = await client.PostAsync(ApiUrl, content);
+
+                // Yanıtı okuma
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                // Önerilen şarkıları işleme
+                dynamic recommendations = JsonConvert.DeserializeObject(responseContent);
+                foreach (var recommendation in recommendations.recommendations)
+                {
+                    string songName = recommendation[0].ToString();
+                    string artistName = recommendation[1].ToString();
+
+                    // Önerilen şarkıları kullanmak için burada işlem yapabilirsiniz
+                    Console.WriteLine("Song: " + songName);
+                    Console.WriteLine("Artist: " + artistName);
+                    Console.WriteLine();
+                }
+            }
+        }
+        public class response_model
+        {
+            public string isim { get; set; }
+            public string sanatci { get; set; }
+        }
+    }
+}
